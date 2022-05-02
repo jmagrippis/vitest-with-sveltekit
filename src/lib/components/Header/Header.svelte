4f571912fd8c9839c50ac550333808ff3e87ad2c
Johnny Magrippis <j@magrippis.com>
2022-05-02 22:41:19 +0200

    ✨  bring in Tailwind to prepare for video recording

---
+++ src/lib/components/Header/Header.svelte
@@ -0,0 +1,7 @@
+<script lang="ts">
+	import logo from './svelte-logo.svg'
+</script>
+
+<header class="text-primary py-4 text-center text-2xl">
+	Vitest with Svelte
+</header>
---
+++ src/routes/__layout.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+	import '../app.css'
+
+	import Header from '$lib/components/Header/Header.svelte'
+</script>
+
+<div id="theme-container">
+	<div id="app-content">
+		<Header />
+
+		<main class="flex flex-grow flex-col items-center justify-center">
+			<slot />
+		</main>
+
+		<footer class="text-center text-lg">
+			<a class="decoration-2" href="https://magrippis.com/tube"
+				>magrippis.com/tube</a
+			>
+			<p>🧑‍💻 ^ More tutorials from Johnny ^ 👩‍💻</p>
+		</footer>
+	</div>
+</div>
